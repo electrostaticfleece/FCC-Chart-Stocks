09a306aef528b42a2bfeccec75b6e7f10bab1446
Trevor <trevorwong00@gmail.com>
2016-10-31 15:41:40 -0700

    Removed unnecessary const

--- app/sockets/bindEmits.js
+++ app/sockets/bindEmits.js
@@ -4,9 +4,8 @@ export default function bindEmits(socket, dispatch) {
   let actionKeys = Object.keys(actionCreators);
 
   actionKeys.forEach((key) => {
-    const actionCreator = actionCreators[key];
-    const action = actionCreator();
-    const { meta, type } = action;
+    const actionCreator = actionCreators[key]();
+    const { meta, type } = actionCreator();
 
     if(meta && meta.serverEmit) {
       socket.on(type, (data) => {
